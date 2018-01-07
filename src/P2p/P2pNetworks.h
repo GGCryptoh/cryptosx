7a06a572cca5553633d0b6eca06a139ad523ffeb
Ubuntu <master@seed1.dvlz2csukq3ulml5avtxd14que.bx.internal.cloudapp.net>
2018-01-07 14:09:06 +0000

    Init

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x41, 0x51, 0x52, 0x51, 0x4f, 0x54, 0x4e, 0x4e, 0x54, 0x54, 0x01, 0x02, 0x12, 0x11, 0x01, 0x11 } };
+}
